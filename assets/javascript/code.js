52af9d8087a011338e15a2e56b5f549acecacab7
John Mateer <jj.mateer@live.com>
2019-08-06 18:26:58 -0700

    Added the mobile menu at smallest breakpoint, increased the height of the banner to make it function as a welcome screen with a button that fades in all the content and hides when pressed.

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,3 +1,13 @@
-$(document).ready(function() {
-    $("#nameTag").delay(1000).animate({"opacity": "1"}, 700);
+$(document).ready(function () {
+    $("#banner").append("<btn id='welcomeBtn'>Welcome</btn>")
+    $("#contentContainer").hide();
+    $('#welcomeBtn').on('click', function() {
+        $("#welcomeBtn").hide();
+        $("#nameTag").delay(500).animate({ "opacity": "1" }, 700);
+        $("#menu").delay(500).animate({ "opacity": "0.75" }, 700);
+        $("#contentContainer").show();
+    });
+    
+    
+
 });
