13166d65998d67a4ad04ab2b818f61eefbb3c60b
John Mateer <jj.mateer@live.com>
2019-08-06 21:18:07 -0700

    Added on click functions for each of the menu buttons that scroll to the designated div on click.

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,26 +1,55 @@
+function hideContent() {
+    $("#aboutme").hide();
+    $("#portfolio").hide();
+    $("#contact").hide();
+}
+function showContent() {
+    $("#aboutme").show();
+    $("#portfolio").show();
+    $("#contact").show();
+}
 $(document).ready(function () {
     $("#banner").prepend("<btn id='welcomeBtn'>Welcome</btn>")
-    $("#contentContainer").hide();
+    $("#welcomeBtn").delay(500).animate({ "opacity": "1" }, 700);
+    hideContent();
     $("#banner").css("width", "100%");
     $('#welcomeBtn').on('click', function () {
         $("#welcomeBtn").hide();
         $("#nameTag").delay(500).animate({ "opacity": "1" }, 700);
         $(".menu").delay(500).animate({ "opacity": "0.75" }, 700);
         $("#mobileMenu").delay(500).animate({ "opacity": "1" }, 700);
-        $("#contentContainer").show();
+        showContent();
         $('#banner').css({
             'background-color': 'teal',
         });
+    
     });
 });
+$("#scroll1").on('click',function() {
+    $('body').animate({
+        scrollTop: $("#banner").offset().top
+    },
+        'slow');
+});
+$("#scroll2").on('click',function() {
+    $('body').animate({
+        scrollTop: $("#aboutme").offset().top
+    },
+        'slow');
+});
 
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-  }
+$("#scroll3").on('click',function() {
+    $('body').animate({
+        scrollTop: $("#portfolio").offset().top
+    },
+        'slow');
+});
+
+$("#scroll4").on('click',function() {
+    $('body').animate({
+        scrollTop: $("#contact").offset().top
+    },
+        'slow');
+});
+
 
