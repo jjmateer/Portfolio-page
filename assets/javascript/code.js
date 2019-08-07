13b545b3b73588a908f144263ad347c915fbb63c
John Mateer <jj.mateer@live.com>
2019-08-06 20:43:09 -0700

    Replaced the side bar menu with buttons that are partially off screen and animate onto the screen on hover.

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,13 +1,26 @@
 $(document).ready(function () {
-    $("#banner").append("<btn id='welcomeBtn'>Welcome</btn>")
+    $("#banner").prepend("<btn id='welcomeBtn'>Welcome</btn>")
     $("#contentContainer").hide();
-    $('#welcomeBtn').on('click', function() {
+    $("#banner").css("width", "100%");
+    $('#welcomeBtn').on('click', function () {
         $("#welcomeBtn").hide();
         $("#nameTag").delay(500).animate({ "opacity": "1" }, 700);
-        $("#menu").delay(500).animate({ "opacity": "0.75" }, 700);
+        $(".menu").delay(500).animate({ "opacity": "0.75" }, 700);
+        $("#mobileMenu").delay(500).animate({ "opacity": "1" }, 700);
         $("#contentContainer").show();
+        $('#banner').css({
+            'background-color': 'teal',
+        });
     });
-    
-    
-
 });
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
+
