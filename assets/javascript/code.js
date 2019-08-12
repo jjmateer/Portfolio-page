27a3545c17ec8fec97a5670b154559c82fdff483
John Mateer <jj.mateer@live.com>
2019-08-11 23:49:51 -0700

    AOS wont open untill console is opened in chrome. I wrapped the entire javascript file in the document ready function to see if this changes anything.

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,16 +1,16 @@
-function hideContent() {
-    $("#aboutme").hide();
-    $("#portfolio").hide();
-    $("#contact").hide();
-}
-function showContent() {
-    $("#aboutme").show();
-    $("#portfolio").show();
-    $("#contact").show();
-
-}
-document.ontouchmove = function (e) {e.preventDefault();}
 $(document).ready(function () {
+    function hideContent() {
+        $("#aboutme").hide();
+        $("#portfolio").hide();
+        $("#contact").hide();
+    }
+    function showContent() {
+        $("#aboutme").show();
+        $("#portfolio").show();
+        $("#contact").show();
+
+    }
+
     $("#banner").prepend("<div id='welcomeDisplay'>Welcome</div>")
     $("#banner").append("<btn id='welcomeBtn'>Enter site</btn>")
     $("#welcomeDisplay").delay(500).animate({ "opacity": "1" }, 700);
@@ -20,7 +20,7 @@ $(document).ready(function () {
     $('#welcomeBtn').on('click', function () {
         $("#welcomeDisplay").animate({ "opacity": "0" }, 1000);
         $("#welcomeBtn").animate({ "opacity": "0" }, 1000);
-        $("#nameTag").delay(500).animate({ "opacity": "1" }, 1000);
+        $("#nameTag").delay(500).animate({ "opacity": "1" }, 2000);
         $(".menu").delay(500).animate({ "opacity": "0.50" }, 1000);
         $("#mobileMenu").delay(500).animate({ "opacity": "1" }, 1000);
         showContent();
@@ -29,115 +29,112 @@ $(document).ready(function () {
         });
 
     });
-});
-$("#submit").on("click", function () {
-    event.preventDefault();
-    console.log('clicked');
-    var name = $('#name').val();
-    var email = $('#email').val();
-    var message = $("#message").val();
-    console.log(name);
-    console.log(email);
-    console.log(message);
-    if ($('#name').val() < 1 || $('#email').val() < 1 || $("#message").val() < 1) {
-        console.log('required fields empty')
-        $("#name").attr("placeholder", "*Required");
-        $("#email").attr("placeholder", "*Required");
-        $("#message").attr("placeholder", "*Required");
+    $("#aboutme").scroll(function () {
+        alert('working');
+    });
 
-    } else {
+    $("#submit").on("click", function () {
         event.preventDefault();
-        console.log('valid!')
-        $('.formInputs').val('');
-        $("#name").attr("placeholder", "Name");
-        $("#email").attr("placeholder", "Email");
-        $("#message").attr("placeholder", "Message");
-        $.ajax({
-            url: "https://formspree.io/jj.mateer@live.com",
-            method:"POST",
-            data: $(this).serialize(),
-            dataType: 'json'
+        var name = $('#name').val();
+        var email = $('#email').val();
+        var message = $("#message").val();
+        if ($('#name').val() < 1 || $('#email').val() < 1 || $("#message").val() < 1) {
+            $("#name").attr("placeholder", "*Required");
+            $("#email").attr("placeholder", "*Required");
+            $("#message").attr("placeholder", "*Required");
+
+        } else {
+            event.preventDefault();
+            $('.formInputs').val('');
+            $("#name").attr("placeholder", "Name");
+            $("#email").attr("placeholder", "Email");
+            $("#message").attr("placeholder", "Message");
+            $.ajax({
+                url: "https://formspree.io/jj.mateer@live.com",
+                method: "POST",
+                data: $(this).serialize(),
+                dataType: 'json'
+            });
+            event.preventDefault();
+        }
+
+    });
+    $("#scroll1").on('click', function () {
+        $('body').animate({
+            scrollTop: $("#banner").offset().top
+        },
+            'slow');
+    });
+    $("#scroll2").on('click', function () {
+        $('body').animate({
+            scrollTop: $("#aboutme").offset().top
+        },
+            'slow');
+    });
+
+    $("#scroll3").on('click', function () {
+        $('body').animate({
+            scrollTop: $("#portfolio").offset().top
+        },
+            'slow');
+    });
+
+    $("#scroll4").on('click', function () {
+        $('body').animate({
+            scrollTop: $("#contact").offset().top
+        },
+            'slow');
+    });
+    $("#mobile1").on('click', function () {
+        $('body').animate({
+            scrollTop: $("#aboutme").offset().top
+        },
+            'slow');
+        $("#aboutme").prepend("<div id='homeBtnMobile'></div>")
+        $("#homeBtnMobile").text("Home");
+        $("#homeBtnMobile").on('click', function () {
+            $("#homeBtnMobile").remove();
+            $('body').animate({
+                scrollTop: $("#banner").offset().top
+            },
+                'slow');
+
         });
-        event.preventDefault();
-    }
 
-});
-$("#scroll1").on('click', function () {
-    $('body').animate({
-        scrollTop: $("#banner").offset().top
-    },
-        'slow');
-});
-$("#scroll2").on('click', function () {
-    $('body').animate({
-        scrollTop: $("#aboutme").offset().top
-    },
-        'slow');
-});
-
-$("#scroll3").on('click', function () {
-    $('body').animate({
-        scrollTop: $("#portfolio").offset().top
-    },
-        'slow');
-});
-
-$("#scroll4").on('click', function () {
-    $('body').animate({
-        scrollTop: $("#contact").offset().top
-    },
-        'slow');
-});
-$("#mobile1").on('click', function () {
-    $('body').animate({
-        scrollTop: $("#aboutme").offset().top
-    },
-        'slow');
-    $("#aboutme").prepend("<div id='homeBtnMobile'></div>")
-    $("#homeBtnMobile").text("Home");
-    $("#homeBtnMobile").on('click', function () {
-        $("#homeBtnMobile").remove();
+    });
+    $("#mobile2").on('click', function () {
         $('body').animate({
-            scrollTop: $("#banner").offset().top
+            scrollTop: $("#portfolio").offset().top
         },
             'slow');
+        $("#portfolio").prepend("<div id='homeBtnMobile'></div>")
+        $("#homeBtnMobile").text("Home");
+        $("#homeBtnMobile").on('click', function () {
+            $("#homeBtnMobile").remove();
+            $('body').animate({
+                scrollTop: $("#banner").offset().top
+            },
+                'slow');
 
+        });
+
+    });
+    $("#mobile3").on('click', function () {
+        $('body').animate({
+            scrollTop: $("#contact").offset().top
+        },
+            'slow');
+        $("#contact").prepend("<div id='homeBtnMobile'></div>")
+        $("#homeBtnMobile").text("Home");
+        $("#homeBtnMobile").on('click', function () {
+            $("#homeBtnMobile").remove();
+            $('body').animate({
+                scrollTop: $("#banner").offset().top
+            },
+                'slow');
+
+        });
     });
 
 });
-$("#mobile2").on('click', function () {
-    $('body').animate({
-        scrollTop: $("#portfolio").offset().top
-    },
-        'slow');
-    $("#portfolio").prepend("<div id='homeBtnMobile'></div>")
-    $("#homeBtnMobile").text("Home");
-    $("#homeBtnMobile").on('click', function () {
-        $("#homeBtnMobile").remove();
-        $('body').animate({
-            scrollTop: $("#banner").offset().top
-        },
-            'slow');
-
-    });
-
-});
-$("#mobile3").on('click', function () {
-    $('body').animate({
-        scrollTop: $("#contact").offset().top
-    },
-        'slow');
-    $("#contact").prepend("<div id='homeBtnMobile'></div>")
-    $("#homeBtnMobile").text("Home");
-    $("#homeBtnMobile").on('click', function () {
-        $("#homeBtnMobile").remove();
-        $('body').animate({
-            scrollTop: $("#banner").offset().top
-        },
-            'slow');
-
-    });
-});
-
-
 
